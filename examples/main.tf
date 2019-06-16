458d2944b84a733c7ea07c925f04322d0f97e5e9
Anmol Nagpal <ianmolnagpal@gmail.com>
2019-06-16 04:10:18 +0200

    update

--- examples/main.tf
+++ examples/main.tf
@@ -1,33 +1,31 @@
 module "subnets" {
   source = "../../terraform-aws-eks-cluster"
 
+  //source = "https://github.com/clouddrove/terraform-aws-eks-cluster"
+
+  ## Tags
   name        = "eks"
   application = "clouddrove"
   environment = "test"
   enabled     = true
-
-  version = "0.12"
-
-  vpc_cidr_block = "10.0.0.0/16"
-
+  ## Network
+  vpc_cidr_block                  = "10.0.0.0/16"
+  allowed_security_groups_cluster = []
+  allowed_security_groups_worker  = []
+  ## Ec2
   key_name                    = ""
   image_id                    = ""
   instance_type               = "t2.nano"
   max_size                    = "1"
   min_size                    = "1"
-  associate_public_ip_address = ""
-
-  autoscaling_policies_enabled = ""
-
-  allowed_cidr_blocks_cluster     = ""
-  allowed_cidr_blocks_workers     = ""
-  allowed_security_groups_workers = ""
-  allowed_security_groups_cluster = ""
-  wait_for_capacity_timeout       = ""
-  apply_config_map_aws_auth       = ""
-
-  availability_zones                     = "3"
-  cpu_utilization_high_threshold_percent = "50"
-  cpu_utilization_low_threshold_percent  = "10"
-  health_check_type                      = ""
+  associate_public_ip_address = true
+  availability_zones          = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d"]
+  ## Cluster
+  version                   = "0.12"
+  wait_for_capacity_timeout = "10m"
+  apply_config_map_aws_auth = true
+  ## Health Checks
+  cpu_utilization_high_threshold_percent = "80"
+  cpu_utilization_low_threshold_percent  = "20"
+  health_check_type                      = "EC2"
 }
