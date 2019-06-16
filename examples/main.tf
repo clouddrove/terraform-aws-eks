2c9dc33a55fff492f2b503ac4bba5a8ca20a3875
Anmol Nagpal <ianmolnagpal@gmail.com>
2019-06-16 04:15:36 +0200

    update

--- examples/main.tf
+++ examples/main.tf
@@ -1,3 +1,7 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
 module "subnets" {
   source = "../../terraform-aws-eks-cluster"
 
@@ -7,23 +11,23 @@ module "subnets" {
   name        = "eks"
   application = "clouddrove"
   environment = "test"
-  enabled     = true
+  enabled     = "true"
   ## Network
   vpc_cidr_block                  = "10.0.0.0/16"
   allowed_security_groups_cluster = []
-  allowed_security_groups_worker  = []
+  allowed_security_groups_workers = []
   ## Ec2
   key_name                    = ""
   image_id                    = ""
   instance_type               = "t2.nano"
   max_size                    = "1"
   min_size                    = "1"
-  associate_public_ip_address = true
+  associate_public_ip_address = "true"
   availability_zones          = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d"]
   ## Cluster
   version                   = "0.12"
   wait_for_capacity_timeout = "10m"
-  apply_config_map_aws_auth = true
+  apply_config_map_aws_auth = "true"
   ## Health Checks
   cpu_utilization_high_threshold_percent = "80"
   cpu_utilization_low_threshold_percent  = "20"
