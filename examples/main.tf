9ef55890eda43227769e0b45e0d6c7ce2d4f4fc9
Anmol Nagpal <ianmolnagpal@gmail.com>
2019-06-17 02:02:23 +0200

    update

--- examples/main.tf
+++ examples/main.tf
@@ -18,15 +18,14 @@ module "subnets" {
   allowed_security_groups_workers = []
   ## Ec2
   key_name                    = ""
-  image_id                    = ""
+  image_id                    = "ami-0abcb9f9190e867ab"
   instance_type               = "t2.nano"
   max_size                    = "1"
   min_size                    = "1"
   associate_public_ip_address = "true"
   availability_zones          = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d"]
   ## Cluster
-  version                   = "0.12"
-  wait_for_capacity_timeout = "10m"
+  wait_for_capacity_timeout = "15m"
   apply_config_map_aws_auth = "true"
   ## Health Checks
   cpu_utilization_high_threshold_percent = "80"
