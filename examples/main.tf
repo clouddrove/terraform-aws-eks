6055e776cadca60f78f7b9556cbaf8d5a73af0dc
Sohan Yadav <sohan@clouddrove.com>
2019-06-17 17:26:14 +0530

    update-subnets

--- examples/main.tf
+++ examples/main.tf
@@ -1,29 +1,51 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "eu-west-1"
 }
 
-module "subnets" {
+module "vpc" {
+  source      = "git::https://github.com/clouddrove/terraform-aws-vpc.git?ref=tags/0.11.0"
+  name        = "${var.name}"
+  application = "${var.application}"
+  environment = "${var.environment}"
+  cidr_block  = "${var.vpc_cidr_block}"
+}
+
+module "subnets"  {
+  source              = "../../aws/terraform-aws-pub-pri-subnet"
+  application        = "clouddrove"
+  environment         = "dev"
+  name                = "subnet"
+  availability_zones  = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  vpc_id              = "${module.vpc.vpc_id}"
+  type                = "public-private"
+  igw_id              = "${module.vpc.igw_id}"
+  nat_gateway_enabled = "true"
+  cidr_block    = "${module.vpc.vpc_cidr_block}"
+}
+
+module "eks_cluster" {
   source = "../../terraform-aws-eks-cluster"
 
   //source = "https://github.com/clouddrove/terraform-aws-eks-cluster"
 
   ## Tags
-  name        = "eks"
+  name        = "eks-cluster"
   application = "clouddrove"
-  environment = "test"
+  environment = "dev"
   enabled     = "true"
   ## Network
-  vpc_cidr_block                  = "10.0.0.0/16"
+  vpc_id                  = "${module.vpc.vpc_id}"
+  subnet_ids              = ["${module.subnets.public_subnet_id}"]
   allowed_security_groups_cluster = []
   allowed_security_groups_workers = []
   ## Ec2
-  key_name                    = ""
-  image_id                    = "ami-0abcb9f9190e867ab"
+  key_name                    = "test"
+  image_id                    = "ami-08d658f84a6d84a80"
   instance_type               = "t2.nano"
   max_size                    = "1"
   min_size                    = "1"
   associate_public_ip_address = "true"
-  availability_zones          = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d"]
+  availability_zones          = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
   ## Cluster
   wait_for_capacity_timeout = "15m"
   apply_config_map_aws_auth = "true"
