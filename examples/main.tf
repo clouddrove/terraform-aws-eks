1ea563dea217d5e1160404c7eb92021285892104
Sohan Yadav <sohan@clouddrove.com>
2019-06-29 12:27:38 +0530

    eks

--- examples/main.tf
+++ examples/main.tf
@@ -4,28 +4,29 @@ provider "aws" {
 
 module "vpc" {
   source      = "git::https://github.com/clouddrove/terraform-aws-vpc.git?ref=tags/0.11.0"
-  name        = "${var.name}"
-  application = "${var.application}"
-  environment = "${var.environment}"
-  cidr_block  = "${var.vpc_cidr_block}"
+  name        = var.name
+  application = var.application
+  environment = var.environment
+  cidr_block  = var.vpc_cidr_block
 }
 
-module "subnet" {
-  source = "git::https://github.com/clouddrove/terraform-aws-subnet.git?ref=tags/0.11.0"
+module "subnets" {
+  source = "../../aws/terraform-aws-subnet"  #using ops3 branch
 
-  name               = "${var.name}"
-  application        = "clouddrove"
-  environment        = "test"
-  availability_zones = ["${var.availability_zones}"]
+  name = var.name
+  application = var.application
+  environment = var.environment
 
-  vpc_id     = "${module.vpc.vpc_id}"
-  igw_id     = "${module.vpc.igw_id}"
-  cidr_block = "${module.vpc.vpc_cidr_block}"
-  type       = "public-private"
+  availability_zones = ["us-east-1b", "us-east-1c"]
+  vpc_id = module.vpc.vpc_id
+  cidr_block = module.vpc.vpc_cidr_block
+  type = "public"
+  igw_id = module.vpc.igw_id
+  nat_gateway_enabled = "false"
 }
 
 module "eks-cluster" {
-  source = "git::https://github.com/clouddrove/terraform-aws-eks-cluster?ref=tags/0.11.0"
+  source = "./../"
 
   ## Tags
   name        = "eks"
@@ -39,10 +40,10 @@ module "eks-cluster" {
   allowed_security_groups_workers = []
 
   ## Ec2
-  key_name                    = ""
-  image_id                    = "ami-0abcb9f9190e867ab"
-  instance_type               = "t2.nano"
-  max_size                    = "1"
+  key_name                    = "test"
+  image_id                    = "ami-0200e65a38edfb7e1"
+  instance_type               = "t2.small"
+  max_size                    = "3"
   min_size                    = "1"
   associate_public_ip_address = "true"
   availability_zones          = ["us-east-1a", "us-east-1b"]
@@ -55,6 +56,7 @@ module "eks-cluster" {
   cpu_utilization_high_threshold_percent = "80"
   cpu_utilization_low_threshold_percent  = "20"
   health_check_type                      = "EC2"
-  vpc_id                                 = "${module.vpc.vpc_id}"
-  subnet_ids                             = ["${module.subnet.public_subnet_id}"]
+  vpc_id                                 = module.vpc.vpc_id
+  subnet_ids                             = module.subnets.public_subnet_id
 }
+
