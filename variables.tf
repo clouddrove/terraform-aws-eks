3936c966f5eeb5518464fadef32b8ae221358744
Anmol Nagpal <ianmolnagpal@gmail.com>
2019-12-01 19:41:38 +0100

    fix

--- variables.tf
+++ variables.tf
@@ -247,7 +247,7 @@ variable "max_price" {
 
 variable "spot_instance_type" {
   type        = list(string)
-  default     = []
+  default     = ["t2.medium"]
   description = "Sport instance type to launch."
 }
 
