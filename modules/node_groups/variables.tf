51cff80592ebf931c58eccfc2f1bdc5ec28bd69c
vikrantk-devops <67599710+vikrantk-devops@users.noreply.github.com>
2020-10-05 02:32:17 +0530

    Update variables.tf

--- modules/node_groups/variables.tf
+++ modules/node_groups/variables.tf
@@ -41,3 +41,44 @@ variable "subnet_ids" {
   description = "subnet_ids"
   }
 
+variable "application" {
+  type        = string
+  default     = ""
+  description = "Application (e.g. `cd` or `clouddrove`)."
+}
+
+variable "environment" {
+  type        = string
+  default     = ""
+  description = "Environment (e.g. `prod`, `dev`, `staging`)."
+}
+
+variable "label_order" {
+  type        = list
+  default     = []
+  description = "Label order, e.g. `name`,`application`."
+}
+
+variable "attributes" {
+  type        = list
+  default     = []
+  description = "Additional attributes (e.g. `1`)."
+}
+
+variable "tags" {
+  type        = map
+  default     = {}
+  description = "Additional tags (e.g. map(`BusinessUnit`,`XYZ`)."
+}
+
+variable "managedby" {
+  type        = string
+  default     = "anmol@clouddrove.com"
+  description = "ManagedBy, eg 'CloudDrove' or 'AnmolNagpal'."
+}
+
+variable "delimiter" {
+  type        = string
+  default     = "-"
+  description = "Delimiter to be used between `organization`, `environment`, `name` and `attributes`."
+}
