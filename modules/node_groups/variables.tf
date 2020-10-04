f3e0c177356c85f79026a8058d0b4c82c04b5d37
vikrantk-devops <67599710+vikrantk-devops@users.noreply.github.com>
2020-10-05 01:56:58 +0530

    Create variables.tf

---
+++ modules/node_groups/variables.tf
@@ -0,0 +1,36 @@
+variable "name" {
+  type        = string
+  default     = ""
+  description = "Name  (e.g. `app` or `cluster`)."
+}
+
+variable "node_groups" {
+  type        = list
+  default     = []
+  description = "Name  (e.g. `app` or `cluster`)."
+}
+
+variable "max_size" {
+  type        = list
+  default     = []
+  description = "Name  (e.g. `app` or `cluster`)."
+}
+
+variable "min_size" {
+  type        = list
+  default     = []
+  description = "Name  (e.g. `app` or `cluster`)."
+}
+
+variable "desired_size" {
+  type        = list
+  default     = []
+  description = "Name  (e.g. `app` or `cluster`)."
+}
+
+variable "aws_iam_role_arn" {
+  type        = string
+  default     = ""
+  description = "ARN of EKS iam user"
+
+}
