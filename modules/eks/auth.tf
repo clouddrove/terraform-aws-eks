a4100fdd3ce7b125f6a890c1609bd85170fca9e4
vikrantk-devops <67599710+vikrantk-devops@users.noreply.github.com>
2020-10-07 15:37:54 +0530

    Update auth.tf

--- modules/eks/auth.tf
+++ modules/eks/auth.tf
@@ -93,7 +93,7 @@ provider "kubernetes" {
 }
 
  resource "kubernetes_config_map" "aws_auth_ignore_changes" {
-  count      = var.enabled && var.apply_config_map_aws_auth && var.kubernetes_config_map_ignore_role_changes ? 1 : 0
+  count      = var.enabled && var.apply_config_map_aws_auth ? 1 : 0
   depends_on = [null_resource.wait_for_cluster[0]]
 
   metadata {
@@ -112,18 +112,3 @@ provider "kubernetes" {
   }
 }
 
-resource "kubernetes_config_map" "aws_auth" {
-  count      = var.enabled && var.apply_config_map_aws_auth && var.kubernetes_config_map_ignore_role_changes == false ? 1 : 0
-  depends_on = [null_resource.wait_for_cluster[0]]
-
-  metadata {
-    name      = "aws-auth"
-    namespace = "kube-system"
-  }
-
-  data = {
-    mapRoles    = yamlencode(distinct(concat(local.map_worker_roles, var.map_additional_iam_roles)))
-    mapUsers    = yamlencode(var.map_additional_iam_users)
-    mapAccounts = yamlencode(var.map_additional_aws_accounts)
-  }
-}
