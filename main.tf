eed91f1a88c4660ea54cd2c853cd2b5de71d63af
Anmol Nagpal <anmol@clouddrove.com>
2023-05-12 22:51:26 +0530

    [feature/asg_schedule]Fix: update the tfsec security changes

--- main.tf
+++ main.tf
@@ -29,15 +29,18 @@ resource "aws_eks_cluster" "default" {
   name                      = module.labels.id
   role_arn                  = join("", aws_iam_role.default.*.arn)
   version                   = var.kubernetes_version
-  enabled_cluster_log_types = var.enabled_cluster_log_types #tfsec:ignore:aws_eks_enabled_cluster_log_types
+#tfsec:ignore:aws_eks_enabled_cluster_log_types  
+  enabled_cluster_log_types = var.enabled_cluster_log_types
   tags                      = module.labels.tags
 
 
   vpc_config {    
     subnet_ids              = var.subnet_ids
     endpoint_private_access = var.endpoint_private_access
-    endpoint_public_access  = var.endpoint_public_access  #tfsec:ignore:aws_eks_cluster-endpoint_public_access
-    public_access_cidrs     = var.public_access_cidrs #tfsec:ignore:aws_eks_cluster-public-access-cidr
+#tfsec:ignore:aws_eks_cluster-endpoint_public_access    
+    endpoint_public_access  = var.endpoint_public_access 
+#tfsec:ignore:aws_eks_cluster-public-access-cidr    
+    public_access_cidrs     = var.public_access_cidrs
     security_group_ids      = var.eks_additional_security_group_ids
   }
 
