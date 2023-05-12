d830105ae44ae6e03c4e8f44587b41f1fffcdf2a
Anmol Nagpal <anmol@clouddrove.com>
2023-05-12 19:35:31 +0530

    [feature/asg_schedule]Fix: updated the main.tf

--- main.tf
+++ main.tf
@@ -33,7 +33,8 @@ resource "aws_eks_cluster" "default" {
   tags                      = module.labels.tags
 
 
-  vpc_config {    subnet_ids              = var.subnet_ids
+  vpc_config {    
+    subnet_ids              = var.subnet_ids
     endpoint_private_access = var.endpoint_private_access
     endpoint_public_access  = var.endpoint_public_access
     public_access_cidrs     = var.public_access_cidrs
