416b3a69851bd662faa42ddda561331df3f12c11
Himanshu Ahirwar <himanshu.ahirwar@clouddrove.com>
2023-08-04 23:19:26 +0530

    feat: added default eks addons fix: ebs encryption setting feat: additional security-group

--- locals.tf
+++ locals.tf
@@ -4,7 +4,8 @@ locals {
     resources        = var.cluster_encryption_config_resources
     provider_key_arn = var.enabled ? join("", aws_kms_key.cluster.*.arn) : null
   }
-  aws_policy_prefix = format("arn:%s:iam::aws:policy", join("", data.aws_partition.current.*.partition))
+  aws_policy_prefix             = format("arn:%s:iam::aws:policy", join("", data.aws_partition.current.*.partition))
+  create_outposts_local_cluster = length(var.outpost_config) > 0
 
 }
 
