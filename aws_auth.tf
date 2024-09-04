a48263e285534befc17e6556bcf042688dccab00
Nilesh Gadgi <gadginileshp@gmail.com>
2024-09-05 04:43:43 +0530

    FIX: fix data block, data block was calling before eks cluster creation (#66)
    * FIX: fix data block, data block was calling before eks cluster creation
    
    * FIX: add dependency on eks_auth data block

--- aws_auth.tf
+++ aws_auth.tf
@@ -74,7 +74,7 @@ resource "null_resource" "wait_for_cluster" {
 
 data "aws_eks_cluster" "eks" {
   count = var.enabled && var.apply_config_map_aws_auth ? 1 : 0
-  name  = module.labels.id
+  name  = aws_eks_cluster.default[0].id
 }
 
 # Get an authentication token to communicate with the EKS cluster.
@@ -84,7 +84,7 @@ data "aws_eks_cluster" "eks" {
 # https://www.terraform.io/docs/providers/aws/d/eks_cluster_auth.html
 data "aws_eks_cluster_auth" "eks" {
   count = var.enabled && var.apply_config_map_aws_auth ? 1 : 0
-  name  = module.labels.id
+  name  = aws_eks_cluster.default[0].id
 }
 
 provider "kubernetes" {
