68c26202d6be1a6b648487d8f96cc8e8deac06d3
Aman Verma <141578852+amanverma678@users.noreply.github.com>
2024-11-19 17:52:24 +0530

    fix: warnings

--- versions.tf
+++ versions.tf
@@ -1,12 +1,15 @@
 # Terraform version
 terraform {
   required_version = ">= 1.5.4"
-  kubernetes = ">= 2.33.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = ">= 5.11.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.33.0" # Specify the appropriate version
     }
   }
 }
