609398ac2202f4e57c9771a37c413743100f8a14
Aman Verma <141578852+amanverma678@users.noreply.github.com>
2024-11-19 16:39:40 +0530

    fix: warnings

--- versions.tf
+++ versions.tf
@@ -7,21 +7,5 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 5.11.0"
     }
-    template = {
-      source  = "hashicorp/template"
-      version = ">= 2.2.0" # Update to the minimum required version
-    }
-    null = {
-      source  = "hashicorp/null"
-      version = ">= 3.2.3" # Update to the minimum required version
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">= 2.33.0" # Update to the minimum required version
-    }
-    tls = {
-      source  = "hashicorp/tls"
-      version = ">= 4.0.6" # Specify the appropriate version
-    }
   }
 }
