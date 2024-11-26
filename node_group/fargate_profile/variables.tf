d4a1bbf059c985c236c5e00b0796c1eebe0e0997
amanverma678 <aman.verma@gmail.com>
2024-11-26 17:00:43 +0530

    Fix: warnings

--- node_group/fargate_profile/variables.tf
+++ node_group/fargate_profile/variables.tf
@@ -24,12 +24,6 @@ variable "attributes" {
   description = "Additional attributes (e.g. `1`)."
 }
 
-variable "tags" {
-  type        = map(any)
-  default     = {}
-  description = "Additional tags (e.g. map(`BusinessUnit`,`XYZ`)."
-}
-
 variable "managedby" {
   type        = string
   default     = "hello@clouddorve.com"
