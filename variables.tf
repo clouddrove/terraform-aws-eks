ec8fa51b1858052bc106274355b28e297abfab44
amanverma678 <aman.verma@gmail.com>
2024-12-07 02:41:59 +0530

    FIx: format

--- variables.tf
+++ variables.tf
@@ -140,16 +140,6 @@ variable "outpost_config" {
   default     = {}
 }
 
-variable "instance_market_options" {
-  type = object({
-    market_type = string
-    spot_options = optional(object({
-      max_price = string
-    }))
-  })
-  default = null
-}
-
 #-----------------------------------------------------------KMS---------------------------------------------------------
 variable "cluster_encryption_config_enabled" {
   type        = bool
