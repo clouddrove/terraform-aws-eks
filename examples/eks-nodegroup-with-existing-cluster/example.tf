a12ab4bac8b5983001747e066b01c6bda2642e1d
Arzianghanchi <arzian.ghanchi@clouddrove.com>
2025-08-12 22:29:23 +0000

    feat(eks): add IAM role and policies for attaching managed node group iam to existing EKS cluster

--- examples/eks-nodegroup-with-existing-cluster/example.tf
+++ examples/eks-nodegroup-with-existing-cluster/example.tf
@@ -4,6 +4,7 @@
 locals {
   eks_cluster_name = "clouddrove-eks"
   region           = "us-east-2"
+  environment      = "test"
 }
 
 ##--------------------------------------------------------------------
@@ -35,9 +36,63 @@ data "aws_eks_cluster_auth" "this" {
   name = data.aws_eks_cluster.this.name
 }
 
+data "aws_iam_policy_document" "default" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "amazon_eks_node_group_autoscaler_policy" {
+  statement {
+    actions = [
+      "autoscaling:DescribeAutoScalingGroups",
+      "autoscaling:DescribeAutoScalingInstances",
+      "autoscaling:DescribeLaunchConfigurations",
+      "autoscaling:DescribeTags",
+      "autoscaling:SetDesiredCapacity",
+      "autoscaling:TerminateInstanceInAutoScalingGroup",
+      "autoscaling:TerminateInstanceInAutoScalingGroup",
+      "ec2:DescribeLaunchTemplateVersions",
+      "ecr:*"
+    ]
+    effect    = "Allow"
+    resources = ["*"]
+  }
+}
+
+
+
 ##--------------------------------------------------------------------
 ## MODULE CALL
 ##--------------------------------------------------------------------
+
+module "node-group-role" {
+  source  = "clouddrove/iam-role/aws"
+  version = "1.3.2"
+
+  name        = "${local.eks_cluster_name}-node-group"
+  environment = local.environment
+
+  # Allow EC2 to assume role
+  assume_role_policy = data.aws_iam_policy_document.default.json
+  policy             = data.aws_iam_policy_document.amazon_eks_node_group_autoscaler_policy.json
+
+  # Attach managed policies required for EKS worker nodes
+  policy_enabled = true
+
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",
+    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
+    "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
+    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPullOnly"
+  ]
+}
+
 
 # If your base module is disabled
 module "eks" {
@@ -48,7 +103,7 @@ module "eks" {
   subnet_filter_name   = "tag:kubernetes.io/cluster/${local.eks_cluster_name}"
   subnet_filter_values = ["owned", "shared"]
   region               = local.region
-  node_role_arn        = data.aws_eks_cluster.this.role_arn
+  node_role_arn        = module.node-group-role.arn
   subnet_ids           = data.aws_eks_cluster.this.vpc_config[0].subnet_ids
 
   managed_node_group_defaults = {
