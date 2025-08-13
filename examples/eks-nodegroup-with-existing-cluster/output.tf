e7bd23d15dcb3f64bbf3020a9a765602e44dcb1f
Arzianghanchi <arzian.ghanchi@clouddrove.com>
2025-08-13 20:22:01 +0000

    Removed output managed_node_group_security_group_ids

--- examples/eks-nodegroup-with-existing-cluster/output.tf
+++ examples/eks-nodegroup-with-existing-cluster/output.tf
@@ -7,7 +7,3 @@ output "node_group_role_name" {
   description = "Name of the IAM role assigned to the EKS managed node group."
   value       = module.node-group-role.name
 }
-output "managed_node_group_security_group_ids" {
-  description = "Security group IDs attached to the managed node groups."
-  value       = module.eks.managed_node_group_security_group_ids
-}
