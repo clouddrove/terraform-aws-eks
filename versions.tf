aa2f5f1ba5c93922b1af6d81a82514c47f4672ca
h1manshu98 <himanshu.ahirwar@clouddrove.com>
2025-08-15 03:40:31 +0530

    update version.tf to keep required providers

--- versions.tf
+++ versions.tf
@@ -7,26 +7,6 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 5.11.0"
     }
-
-    tls = {
-      source  = "hashicorp/tls"
-      version = ">= 4.0"
-    }
-
-    template = {
-      source  = "hashicorp/template"
-      version = ">= 2.2.0"
-    }
-
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">= 2.20.0"
-    }
-
-    null = {
-      source  = "hashicorp/null"
-      version = "~> 3.2" # or latest stable version you're comfortable with
-    }
   }
 }
 
