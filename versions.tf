f66275319a5ffbfd20c7b660ccc8526cf154d034
Anmol Nagpal <anmolnagpal@users.noreply.github.com>
2026-03-20 07:17:13 -0400

    ⬆️ upgrade: update AWS provider to >= 5.80.0 and Terraform to >= 1.10.0

--- versions.tf
+++ versions.tf
@@ -1,11 +1,11 @@
 # Terraform version
 terraform {
-  required_version = ">= 1.5.4"
+  required_version = ">= 5.80.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.11.0"
+      version = ">= 5.80.0"
     }
   }
 }
