083c8e690b63386bcc9380d7af4a01a10978d0e0
CloudDrove CI <84795582+clouddrove-ci@users.noreply.github.com>
2026-03-20 12:56:59 +0100

    chore: upgrade AWS provider to >= 5.80.0 and Terraform to >= 1.10.0 (#86)
    * ⬆️ upgrade: update AWS provider to >= 5.80.0 and Terraform to >= 1.10.0
    
    * fix: correct required_version to >= 1.10.0
    
    ---------
    
    Co-authored-by: Anmol Nagpal <anmolnagpal@users.noreply.github.com>

--- examples/self_managed/versions.tf
+++ examples/self_managed/versions.tf
@@ -1,14 +1,14 @@
 terraform {
-  required_version = ">= 1.5.4"
+  required_version = ">= 1.10.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.11.0"
+      version = ">= 5.80.0"
     }
     cloudinit = {
       source  = "hashicorp/cloudinit"
-      version = ">= 2.0"
+      version = ">= 5.80.0"
     }
   }
 }
